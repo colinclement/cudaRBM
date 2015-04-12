b62e273367abc2e370359b4eab4659d482008a2e
Colin Clement <colin.clement@gmail.com>
2015-04-12 17:03:22 -0400

    Moved memory functions, tested data correlations

--- code/src/main.cu
+++ code/src/main.cu
@@ -12,10 +12,11 @@
 #include "loadSpins.h"
 #include "layers.h"
 #include "sampleCorrelate.h"
+#include "workingMemory.h"
 #include "types.h"
 
 #define DBUG
-//#define DBUG_LOAD
+#define DBUG_LOAD
 
 #ifndef MIN
 #define MIN(a, b) ((a > b) ? b : a)
@@ -23,35 +24,8 @@
 #ifndef MAX
 #define MAX(a, b) ((a > b) ? a : b)
 #endif
-#define IDX2F(i,j,ld) ((((j)-1)*(ld))+((i)-1))
+#define IDX2F(i,j,ld) (((j)*(ld))+(i))
 
-void allocateMemory(float **h_W, float **d_W, 
-		    float **h_modelCorrelations, float **d_modelCorrelations,
-		    float **d_random,
-		    int N_v, int N_h){
-    int WBYTES = N_v * N_h * sizeof(float);
-    *h_W = (float *)malloc(WBYTES);
-    memset(*h_W, 0, WBYTES);
-    *h_modelCorrelations = (float *)malloc(WBYTES);
-    memset(*h_modelCorrelations, 0, WBYTES);
-    checkCudaErrors(cudaMalloc((void **)d_W, WBYTES));
-    checkCudaErrors(cudaMemset(*d_W, 0, WBYTES));
-    checkCudaErrors(cudaMalloc((void **)d_modelCorrelations, WBYTES));
-    checkCudaErrors(cudaMemset(*d_modelCorrelations, 0, WBYTES));
-    checkCudaErrors(cudaMalloc((void **)d_random, sizeof(float)*(N_v+N_h)));
-    checkCudaErrors(cudaMemset(*d_W, 0, sizeof(float)*(N_v+N_h)));
-}
-
-void freeMemory(float **h_W, float **d_W,
-		float **h_modelCorrelations, float **d_modelCorrelations,
-		float **d_random){
-    free(*h_W); *h_W = NULL;
-    free(*h_modelCorrelations), *h_modelCorrelations = NULL; 
-    cudaFree(d_W); cudaFree(d_random);
-    cudaFree(d_modelCorrelations);
-    *d_W = NULL; *d_random = NULL;
-    *d_modelCorrelations = NULL;
-}
 
 int main(int argc, char **argv){
 
@@ -64,12 +38,12 @@ int main(int argc, char **argv){
     int dev = 0; 
     cudaSetDevice(dev);
     const char *printMSG = "Incorrect number of arguments: Usage: \n\
-			    ./curbm filename N_visible N_hidden k_samples\n";
-    if (argc < 5){
+			    ./curbm filename N_visible N_hidden k_samples batchsize\n";
+    if (argc < 6){
         printf("%s", printMSG);
 	return 0;
     }
-    else if (argc > 5){
+    else if (argc > 6){
         printf("%s", printMSG);
         return 0;
     }
@@ -77,6 +51,7 @@ int main(int argc, char **argv){
     int N_v = atoi(argv[2]);
     int N_h = atoi(argv[3]);
     int k = atoi(argv[4]);
+    int batchSize = atoi(argv[5]);
   
     int Nbits = 0;//, numSamples = 0;
 
@@ -99,6 +74,9 @@ int main(int argc, char **argv){
 
     float *h_W, *d_W;
     float *h_modelCorrelations, *d_modelCorrelations, *d_random;
+    float *h_dataCorrelations, *d_dataCorrelations;
+    //For Sampling data distribution
+    float *d_hiddenGivenData, *d_hiddenEnergy, *d_hiddenRandom;
      
 #ifdef DBUG
     printf("Allocating layers\n");
@@ -106,9 +84,12 @@ int main(int argc, char **argv){
 
     allocateLayer(&visible, N_v, k);
     allocateLayer(&hidden, N_h, k);
-    allocateMemory(&h_W, &d_W, 
+    allocateMemory(&h_W, &d_W,
 		   &h_modelCorrelations, &d_modelCorrelations,
-		   &d_random, N_v, N_h);
+		   &h_dataCorrelations, &d_dataCorrelations,
+		   &d_random, &d_hiddenRandom,
+		   &d_hiddenGivenData, &d_hiddenEnergy,
+		   N_v, N_h);
     
     //cuBLAS init
     cublasHandle_t cublasHandle;
@@ -139,24 +120,31 @@ int main(int argc, char **argv){
     checkCudaErrors(cudaDeviceSynchronize());
 */
 
-    float *d_initialVisible;
+    float *d_initialVisible, *d_batch;
     checkCudaErrors(cudaMalloc(&d_initialVisible, visible.BYTES));
-    //Move over first config to device
+    checkCudaErrors(cudaMalloc(&d_batch, visible.BYTES * batchSize)); 
     checkCudaErrors(cudaMemcpy(d_initialVisible, h_spinlist, visible.BYTES, cudaMemcpyHostToDevice));
+    checkCudaErrors(cudaMemcpy(d_batch, h_spinlist, visible.BYTES * batchSize, cudaMemcpyHostToDevice));
+
+    FILE *fpW = fopen("testW.dat", "w");
+    FILE *fph = fopen("testhidden.dat", "w");
+    FILE *fpv = fopen("testvisible.dat", "w");
 
 #ifdef DBUG_LOAD
     printf("visible.BYTES = %d\n", visible.BYTES);
     printf("first spin configuration:\n");
     for (int i=0; i < N_v; i++){
-	if (i % 8 == 0)
-	    printf("\n");
-        printf("%d\t", (int) h_spinlist[i]);
-    } printf("\n");
+	if (i % N_v == 0)
+	    fprintf(fpv, "\n");
+        fprintf(fpv, "%f\t", h_spinlist[i]);
+    } 
 #endif 
 
     checkCudaErrors(cudaEventRecord(start, 0));
     computeK_Gibbs(visible, hidden, d_W, d_initialVisible, d_random, cublasHandle, rng);
     computeModelCorrelations(visible, hidden, d_modelCorrelations, cublasHandle);
+    computeDataCorrelations(d_dataCorrelations, d_W, d_batch, d_hiddenRandom, d_hiddenGivenData, 
+		            d_hiddenEnergy, N_v, N_h, batchSize, cublasHandle, rng);
     checkCudaErrors(cudaEventRecord(stop, 0));
     
     checkCudaErrors(cudaEventSynchronize(stop));
@@ -168,61 +156,80 @@ int main(int argc, char **argv){
     copyLayerDeviceToHost(&hidden);
     checkCudaErrors(cudaMemcpy(h_modelCorrelations, d_modelCorrelations, 
 			       sizeof(float)*N_v*N_h, cudaMemcpyDeviceToHost));
+    checkCudaErrors(cudaMemcpy(h_W, d_W, sizeof(float)*N_v*N_h, cudaMemcpyDeviceToHost));
+
 
 #ifdef DBUG
-    printf("model correlations = ");
-    for (int i=0; i < N_v*N_h; i++){
-        if (i % N_h ==0)
-            printf("\n");
-	printf("%f\t", h_modelCorrelations[i]);
+    //printf("model correlations = ");
+    for (int i=0; i < N_v; i++){
+        fprintf(fpW, "\n");
+        for (int j=0; j < N_h; j++){
+            fprintf(fpW, "%f\t", h_W[IDX2F(i,j, N_v)]);
+        }
     }
-    printf("\nHidden sample = ");
+    for (int i=0; i < N_v; i++){
+        fprintf(fpW, "\n");
+        for (int j=0; j < N_h; j++){
+            fprintf(fpW, "%f\t", h_modelCorrelations[IDX2F(i,j, N_v)]);
+        }
+    }
+    //printf("\nHidden sample = ");
+    int nhiddens = hidden.kSamples * N_h;
+    for (int j=0; j < nhiddens; j++){
+	if (j % N_h ==0)
+	    fprintf(fph, "\n");
+	fprintf(fph, "%f\t", hidden.h_samples[j]);
+    }
+    //printf("\nHidden Conditional Probability = ");
     for (int j=0; j < N_h; j++){
-	if (j % N_v ==0)
-	    printf("\n");
-	printf("%f\t", hidden.h_samples[j]);
+	if (j % N_h ==0)
+	    fprintf(fph, "\n");
+	fprintf(fph, "%f\t", hidden.h_conditionalP[j]);
     }
-    printf("\nHidden Conditional Probability = ");
+    //printf("\nHidden Energies = ");
     for (int j=0; j < N_h; j++){
-	if (j % N_v ==0)
-	    printf("\n");
-	printf("%f\t", hidden.h_conditionalP[j]);
+	if (j % N_h ==0)
+	    fprintf(fph, "\n");
+	fprintf(fph, "%f\t", hidden.h_energySum[j]);
     }
-    printf("\nHidden Energies = ");
-    for (int j=0; j < N_h; j++){
-	if (j % N_v ==0)
-	    printf("\n");
-	printf("%f\t", hidden.h_energySum[j]);
-    }
-    printf("\nVisible sample = ");
-    for (int i=0; i < N_h; i++){
-	if (i % N_h == 0){
-	    printf("\n");
+    int nvisibles = visible.kSamples * N_v;
+    //printf("\nVisible sample = ");
+    for (int i=0; i < nvisibles; i++){
+	if (i % N_v == 0){
+	    fprintf(fpv, "\n");
 	}
-	printf("%f\t", visible.h_samples[i]);
+	fprintf(fpv, "%f\t", visible.h_samples[i]);
     }
-    printf("\nVisible Conditional Probability = ");
-    for (int i=0; i < N_h; i++){
-	if (i % N_h == 0){
-	    printf("\n");
+    //printf("\nVisible Conditional Probability = ");
+    for (int i=0; i < N_v; i++){
+	if (i % N_v == 0){
+	    fprintf(fpv, "\n");
 	}
-	printf("%f\t", visible.h_conditionalP[i]);
+	fprintf(fpv, "%f\t", visible.h_conditionalP[i]);
     }
-    printf("\nVisible energies = ");
-    for (int i=0; i < N_h; i++){
-	if (i % N_h == 0){
-	    printf("\n");
+    //printf("\nVisible energies = ");
+    for (int i=0; i < N_v; i++){
+	if (i % N_v == 0){
+	    fprintf(fpv, "\n");
 	}
-	printf("%f\t", visible.h_energySum[i]);
+	fprintf(fpv, "%f\t", visible.h_energySum[i]);
     }
-    printf("\n");
+    fclose(fpW);
+    fclose(fph);
+    fclose(fpv);
+
+    //printf("\n");
 #endif
     
     checkCudaErrors(cublasDestroy(cublasHandle));
     checkCudaErrors(curandDestroyGenerator(rng));
+
     freeLayer(visible); freeLayer(hidden); 
-    freeMemory(&h_W, &d_W, &h_modelCorrelations, &d_modelCorrelations,
-	       &d_random);
+    freeMemory(&h_W, &d_W, 
+	       &h_modelCorrelations, &d_modelCorrelations,
+	       &h_dataCorrelations, &d_dataCorrelations,
+	       &d_random, &d_hiddenRandom, 
+	       &d_hiddenGivenData, &d_hiddenEnergy);
 
     return EXIT_SUCCESS;
 }
