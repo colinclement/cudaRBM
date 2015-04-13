23554001e5dfb7f914821152d848a6638b8ff662
Colin Clement <colin.clement@gmail.com>
2015-04-13 17:28:43 -0400

    Added monitoring of previous step

--- code/src/workingMemory.cu
+++ code/src/workingMemory.cu
@@ -3,22 +3,23 @@
 #include <helper_cuda.h>
 
 
-void allocateMemory(float **h_W, float **d_W, 
+void allocateMemory(float **h_W, float **d_W, float **d_previousWstep, 
 		    float **h_modelCorrelations, float **d_modelCorrelations,
 		    float **h_dataCorrelations, float **d_dataCorrelations,
 		    float **d_random, int N_v, int N_h){
     int WBYTES = N_v * N_h * sizeof(float);
-    int HBYTES = N_h * sizeof(float);
     *h_W = (float *)malloc(WBYTES);
     memset(*h_W, 0, WBYTES);
     //These things can probably go after debugging is done
     *h_modelCorrelations = (float *)malloc(WBYTES);
     memset(*h_modelCorrelations, 0, WBYTES);
-    *h_dataCorrelations = (float *)malloc(HBYTES);
-    memset(*h_dataCorrelations, 0, HBYTES);
+    *h_dataCorrelations = (float *)malloc(WBYTES);
+    memset(*h_dataCorrelations, 0, WBYTES);
 
     checkCudaErrors(cudaMalloc((void **)d_W, WBYTES));
     checkCudaErrors(cudaMemset(*d_W, 0, WBYTES));
+    checkCudaErrors(cudaMalloc((void **)d_previousWstep, WBYTES));
+    checkCudaErrors(cudaMemset(*d_previousWstep, 0, WBYTES));
     checkCudaErrors(cudaMalloc((void **)d_modelCorrelations, WBYTES));
     checkCudaErrors(cudaMemset(*d_modelCorrelations, 0, WBYTES));
     checkCudaErrors(cudaMalloc((void **)d_dataCorrelations, WBYTES));
@@ -28,14 +29,14 @@ void allocateMemory(float **h_W, float **d_W,
     checkCudaErrors(cudaMemset(*d_random, 0, sizeof(float)*(N_v+N_h)));
 }
 
-void freeMemory(float **h_W, float **d_W,
+void freeMemory(float **h_W, float **d_W, float **d_previousWstep,
 		float **h_modelCorrelations, float **d_modelCorrelations,
 		float **h_dataCorrelations, float **d_dataCorrelations,
 		float **d_random){
     free(*h_W); *h_W = NULL;
     free(*h_modelCorrelations), *h_modelCorrelations = NULL; 
     cudaFree(d_W); cudaFree(d_random); cudaFree(d_modelCorrelations);
-    cudaFree(d_dataCorrelations); 
-    *d_W = NULL; *d_random = NULL; 
+    cudaFree(d_dataCorrelations); cudaFree(d_previousWstep);
+    *d_W = NULL; *d_random = NULL; *d_previousWstep = NULL; 
     *d_modelCorrelations = NULL; *d_dataCorrelations = NULL; 
 }
--- code/src/workingMemory.h
+++ code/src/workingMemory.h
@@ -1,12 +1,12 @@
 #ifndef __WORKINGMEMORY_H__
 #define __WORKINGMEMORY_H__
 
-void allocateMemory(float **h_W, float **d_W, 
+void allocateMemory(float **h_W, float **d_W, float **d_previousWstep, 
 		    float **h_modelCorrelations, float **d_modelCorrelations,
 		    float **h_dataCorrelations, float **d_dataCorrelations,
 		    float **d_random, int N_v, int N_h);
 
-void freeMemory(float **h_W, float **d_W,
+void freeMemory(float **h_W, float **d_W, float **d_previousWstep,
 		float **h_modelCorrelations, float **d_modelCorrelations,
 		float **h_dataCorrelations, float **d_dataCorrelations,
 		float **d_random);
