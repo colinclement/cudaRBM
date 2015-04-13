f97a5704ccb569edebb514a22b7a49ae6593d388
Colin Clement <colin.clement@gmail.com>
2015-04-13 13:22:42 -0400

    updated allocations after putting data correlations into struct

--- code/src/workingMemory.cu
+++ code/src/workingMemory.cu
@@ -6,9 +6,7 @@
 void allocateMemory(float **h_W, float **d_W, 
 		    float **h_modelCorrelations, float **d_modelCorrelations,
 		    float **h_dataCorrelations, float **d_dataCorrelations,
-		    float **d_random, float **d_hiddenRandom,
-		    float **d_hiddenGivenData, float **d_hiddenEnergy,
-		    int N_v, int N_h){
+		    float **d_random, int N_v, int N_h){
     int WBYTES = N_v * N_h * sizeof(float);
     int HBYTES = N_h * sizeof(float);
     *h_W = (float *)malloc(WBYTES);
@@ -25,28 +23,19 @@ void allocateMemory(float **h_W, float **d_W,
     checkCudaErrors(cudaMemset(*d_modelCorrelations, 0, WBYTES));
     checkCudaErrors(cudaMalloc((void **)d_dataCorrelations, WBYTES));
     checkCudaErrors(cudaMemset(*d_dataCorrelations, 0, WBYTES));
-    checkCudaErrors(cudaMalloc((void **)d_hiddenGivenData, HBYTES));
-    checkCudaErrors(cudaMemset(*d_hiddenGivenData, 0, HBYTES));
-    checkCudaErrors(cudaMalloc((void **)d_hiddenEnergy, HBYTES));
-    checkCudaErrors(cudaMemset(*d_hiddenEnergy, 0, HBYTES));
-
+    
     checkCudaErrors(cudaMalloc((void **)d_random, sizeof(float)*(N_v+N_h)));
     checkCudaErrors(cudaMemset(*d_random, 0, sizeof(float)*(N_v+N_h)));
-    checkCudaErrors(cudaMalloc((void **)d_hiddenRandom, HBYTES));
-    checkCudaErrors(cudaMemset(*d_hiddenRandom, 0, HBYTES));
 }
 
 void freeMemory(float **h_W, float **d_W,
 		float **h_modelCorrelations, float **d_modelCorrelations,
 		float **h_dataCorrelations, float **d_dataCorrelations,
-		float **d_random, float **d_hiddenRandom,
-		float **d_hiddenGivenData, float **d_hiddenEnergy){
+		float **d_random){
     free(*h_W); *h_W = NULL;
     free(*h_modelCorrelations), *h_modelCorrelations = NULL; 
-    cudaFree(d_W); cudaFree(d_random); cudaFree(d_hiddenRandom);
-    cudaFree(d_modelCorrelations); cudaFree(d_hiddenGivenData);
-    cudaFree(d_dataCorrelations); cudaFree(d_hiddenEnergy);
-    *d_W = NULL; *d_random = NULL; *d_hiddenRandom = NULL;
-    *d_modelCorrelations = NULL; d_hiddenGivenData = NULL;
-    *d_dataCorrelations = NULL; *d_hiddenEnergy = NULL;
+    cudaFree(d_W); cudaFree(d_random); cudaFree(d_modelCorrelations);
+    cudaFree(d_dataCorrelations); 
+    *d_W = NULL; *d_random = NULL; 
+    *d_modelCorrelations = NULL; *d_dataCorrelations = NULL; 
 }
--- code/src/workingMemory.h
+++ code/src/workingMemory.h
@@ -4,14 +4,11 @@
 void allocateMemory(float **h_W, float **d_W, 
 		    float **h_modelCorrelations, float **d_modelCorrelations,
 		    float **h_dataCorrelations, float **d_dataCorrelations,
-		    float **d_random, float **d_hiddenRandom,
-		    float **d_hiddenGivenData, float **d_hiddenEnergy,
-		    int N_v, int N_h);
+		    float **d_random, int N_v, int N_h);
 
 void freeMemory(float **h_W, float **d_W,
 		float **h_modelCorrelations, float **d_modelCorrelations,
 		float **h_dataCorrelations, float **d_dataCorrelations,
-		float **d_random, float **d_hiddenRandom,
-		float **d_hiddenGivenData, float **d_hiddenEnergy);
+		float **d_random);
 
 #endif
