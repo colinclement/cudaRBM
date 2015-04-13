b7723bc1f3c241a3bfd6ff1321230d9996abc949
Colin Clement <colin.clement@gmail.com>
2015-04-13 13:21:45 -0400

    Made loaded spins allocate page-locked memory

--- code/src/loadSpins.cu
+++ code/src/loadSpins.cu
@@ -3,6 +3,9 @@
 #include <string.h>
 #include <errno.h>
 
+#include <cuda.h>
+#include <helper_cuda.h>
+
 //#define DBUG
 
 float* loadSpins(const char *filename, int *Nbits) {
@@ -17,29 +20,35 @@ float* loadSpins(const char *filename, int *Nbits) {
     else{
 
         int ch = 0;
-        int n=0, Nbytes=-1;
+        int Nbytes=-1;
        
         while(!feof(fp)){
             ch = fgetc(fp);
-    	Nbytes++;
+    	    Nbytes++;
         };
     
 #ifdef DBUG
         printf("Loaded %d bytes\n", Nbytes);
 #endif 
 
-        float *spins = (float *)malloc(Nbytes * 8 * sizeof(int));
-        rewind(fp);
+        //float *spins = (float *)malloc(Nbytes * 8 * sizeof(int));
+	float *h_spins;
+        cudaError_t status = cudaMallocHost((void **)&h_spins, Nbytes*8*sizeof(float));
+	if (status != cudaSuccess){
+	    printf("Error allocating pinned host memory\n");
+	    h_spins = (float *)malloc(Nbytes * 8 * sizeof(float));
+	}	
+	rewind(fp);
         *Nbits = Nbytes * 8;//Total sample size
 
         for (int t=0; t < Nbytes; t++){
             ch = fgetc(fp);
             for (int i=0; i < 8; i++){
 	        int bit = (ch & (1 << (7-i))) >> (7-i);
-                spins[8*t + i] = 2 * bit - 1; 
+                h_spins[8*t + i] = 2 * bit - 1; 
             }
         }
         fclose(fp);
-        return spins;
+        return h_spins;
     }
 }
