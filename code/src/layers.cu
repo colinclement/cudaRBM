ae55ed0473bb115f8d75517edb78c30529bcb23e
Colin Clement <colin.clement@gmail.com>
2015-04-28 16:22:13 -0400

    added connection struct methods

--- code/src/layers.cu
+++ code/src/layers.cu
@@ -1,7 +1,7 @@
 #include <stdlib.h>
 #include <cuda.h>
 #include <helper_cuda.h>
-
+#include <curand.h>
 #include "types.h"
 
 __host__
@@ -49,6 +49,53 @@ Layer allocateLayer(int N_units, int numSamples){
 }
 
 __host__
+Connection allocateConnection(int N_v, int N_h, curandGenerator_t rng,
+                              Filter filterType, int width, int stride){ 
+    Connection newConn;
+    newConn.fan_in = N_v; newConn.fan_out = N_h;
+    newConn.filterType = filterType;
+    switch(filterType){
+        case ALLTOALL:
+            newConn.rows = N_v; newConn.cols = N_h;
+            newConn.width = 0; newConn.stride = 1;
+            break;
+        case CONVOLUTION:
+            newConn.rows = width; newConn.cols = width;
+            newConn.width = width; newConn.stride = stride;
+            break;
+        default:
+            break;
+    }
+    newConn.FILTERBYTES = newConn.rows * newConn.cols * sizeof(float);
+    newConn.CORRBYTES = sizeof(float) * N_v * N_h;
+    int FBYTES = newConn.FILTERBYTES;
+    int CBYTES = newConn.CORRBYTES;
+
+    newConn.h_W = (float *)malloc(FBYTES);
+    memset(newConn.h_W, 0, FBYTES);
+    newConn.h_modelCorrelations = (float *)malloc(CBYTES);
+    memset(newConn.h_modelCorrelations, 0, CBYTES);
+    newConn.h_dataCorrelations = (float *)malloc(CBYTES);
+    memset(newConn.h_dataCorrelations, 0, CBYTES);
+    
+    checkCudaErrors(cudaMalloc((void **)&newConn.d_W, FBYTES));
+    checkCudaErrors(cudaMemset(newConn.d_W, 0, FBYTES));
+    checkCudaErrors(cudaMalloc((void **)&newConn.d_modelCorrelations, CBYTES));
+    checkCudaErrors(cudaMemset(newConn.d_modelCorrelations, 0, CBYTES));
+    checkCudaErrors(cudaMalloc((void **)&newConn.d_dataCorrelations, CBYTES));
+    checkCudaErrors(cudaMemset(newConn.d_dataCorrelations, 0, CBYTES));
+
+    float stdDev = 1.f/sqrt( (float) newConn.cols); 
+    curandGenerateNormal(rng, newConn.d_W, (size_t) newConn.rows*newConn.cols, 
+                         0.f, stdDev);
+    //Copy initial weights to device
+    checkCudaErrors(cudaMemcpy(newConn.h_W, newConn.d_W, 
+                               FBYTES, cudaMemcpyDeviceToHost));
+
+    return newConn;
+}
+
+__host__
 void updateLayerSample(Layer unitLayer, float *h_hostSamples, int  BYTES,
                        cudaStream_t stream){
     checkCudaErrors(cudaMemcpyAsync(unitLayer.d_samples, h_hostSamples, BYTES,
@@ -66,4 +113,19 @@ void freeLayer(Layer newLayer){
     cudaFree(newLayer.d_energySum); newLayer.d_energySum=NULL;
     free(newLayer.h_random); newLayer.h_random = NULL;
 }
+
+__host__
+void freeConnection(Connection newConn){
+    free(newConn.h_W); newConn.h_W = NULL;
+    cudaFree(newConn.d_W); newConn.d_W = NULL;
+    free(newConn.h_modelCorrelations);
+    newConn.h_modelCorrelations = NULL;
+    cudaFree(newConn.d_modelCorrelations);
+    newConn.d_modelCorrelations = NULL;
+    free(newConn.h_dataCorrelations);
+    newConn.h_dataCorrelations = NULL;
+    cudaFree(newConn.d_dataCorrelations);
+    newConn.d_dataCorrelations = NULL;
+}
+
 
--- code/src/layers.h
+++ code/src/layers.h
@@ -1,6 +1,8 @@
 #ifndef __LAYERS_H__
 #define __LAYERS_H__
 
+#include <cuda.h>
+#include <curand.h>
 #include "types.h"
 
 __host__
@@ -10,10 +12,16 @@ __host__
 Layer allocateLayer(int N_units, int numSamples);
 
 __host__
+Connection allocateConnection(int N_v, int N_h, curandGenerator_t rng,
+                              Filter filterType, int width, int stride);
+__host__
 void updateLayerSample(Layer unitLayer, float *h_hostSamples,
                        int  BYTES, cudaStream_t stream);
 
 __host__
 void freeLayer(Layer newLayer);
+
+__host__
+void freeConnection(Connection newConn);
 
 #endif
